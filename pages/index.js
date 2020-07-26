5e02ddd843ac13a93c88bd019eba9e0282986175
rauleite <raul.abreu.leit@gmail.com>
2020-07-26 11:19:45 -0300

    pre-auth

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import AppDrawerSides from '../components/Canvas';
 
 export default () => (
   <>
-    {/* <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/4.0.0-rc.1/fabric.min.js"
-      integrity="sha512-rKF82ziMDgkkUwTBlsQhy6Dzdyydg5ikvL1zueWJ6SQxzXaqPY85rEiRvJymMI5YiQqyvm0+mlVYb5tLjmslQA=="
-      crossOrigin="anonymous"
-      // async
-    /> */}
     <AppDrawerSides />
   </>
 );
+
+// <script
+//   src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/4.0.0-rc.1/fabric.min.js"
+// eslint-disable-next-line max-len
+//   integrity="sha512-rKF82ziMDgkkUwTBlsQhy6Dzdyydg5ikvL1zueWJ6SQxzXaqPY85rEiRvJymMI5YiQqyvm0+mlVYb5tLjmslQA=="
+//   crossOrigin="anonymous"
+//   // async
+// />
